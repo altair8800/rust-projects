b420fcc7b2dff42795a68618c2b439ef140b4ee2
barco-zaleta <barco.zaleta@xero.com>
2023-05-01 02:00:21 +1200

    Match guess and secret number, parse input safely

---
+++ target/debug/deps/rand_chacha-ff7d4f8ea1b2422f.d
@@ -0,0 +1,9 @@
+/Users/barco.zaleta/temp/rust-projects/target/debug/deps/rand_chacha-ff7d4f8ea1b2422f.rmeta: /Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+
+/Users/barco.zaleta/temp/rust-projects/target/debug/deps/librand_chacha-ff7d4f8ea1b2422f.rlib: /Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+
+/Users/barco.zaleta/temp/rust-projects/target/debug/deps/rand_chacha-ff7d4f8ea1b2422f.d: /Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+
+/Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs:
+/Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs:
+/Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs:
---
+++ target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/barco.zaleta/temp/rust-projects/target/debug/guessing_game: /Users/barco.zaleta/temp/rust-projects/src/main.rs
