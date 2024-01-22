75449f34f293741caebcd1ea953cc53213f570e2
Barco <barco@Barcos-MacBook-Air.local>
2024-01-22 23:27:21 +0100

    Remove generated files

--- target/debug/deps/cfg_if-92c7aa6050e83454.d
+++
@@ -1,7 +0,0 @@
-/Users/barco.zaleta/temp/rust-projects/target/debug/deps/cfg_if-92c7aa6050e83454.rmeta: /Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/Users/barco.zaleta/temp/rust-projects/target/debug/deps/libcfg_if-92c7aa6050e83454.rlib: /Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/Users/barco.zaleta/temp/rust-projects/target/debug/deps/cfg_if-92c7aa6050e83454.d: /Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/Users/barco.zaleta/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
--- target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/barco.zaleta/temp/rust-projects/target/debug/guessing_game: /Users/barco.zaleta/temp/rust-projects/src/main.rs
